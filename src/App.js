9bba9b43fc522c76e653cb83a3571aee69a5ed0b
DanielM <davidmaestr171@gmail.com>
2023-11-27 17:47:23 -0500

    mi-app-react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Message from "./message";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hola desde reacy</h1>
+      <Message name={'duviel'}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/message.js
@@ -0,0 +1,8 @@
+const Message = ({name}) => {
+  console.log(name);
+  return (
+    <h2>Este es el mensaje del componente {name} </h2>
+  )
+}
+
+export default Message;
